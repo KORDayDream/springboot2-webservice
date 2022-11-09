23b4a629d4865b5098a2e1c2750ce23f3369243a
KORDayDream <hojoong1234@naver.com>
2022-11-09 23:43:59 +0900

    Added LoginUser Annotation and changed related Annotation addressing sessionuser logic

--- src/main/java/com/jojoidu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoidu/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.jojoidu.book.springboot.web;
 
+import com.jojoidu.book.springboot.config.auth.LoginUser;
+import com.jojoidu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoidu.book.springboot.service.posts.PostsService;
 import com.jojoidu.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -14,8 +16,11 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
